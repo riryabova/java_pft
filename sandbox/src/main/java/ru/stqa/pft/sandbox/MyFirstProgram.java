1a673e8b7a31795cb166c66ed978277c4dff4f94
Rita Ryabova <riryabova@mail.ru>
2018-06-10 16:06:59 +0500

    Добавлены функции вычисления площади

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,6 +2,23 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("world");
+    hello("Rita");
+    double l = 6;
+    double m = 7;
+    System.out.println("Площадь квадрата со стороной "+l+" = "+ area(l));
+    System.out.println("Площадь прямоугольника со сторонами "+l+" и "+m+" = "+ area(l,m));
+  }
+
+  public static void hello(String sombody) {
+    System.out.println("Hello, " + sombody + " !");
+  }
+
+  public static double area(double l){
+    return l*l;
+  }
+
+  public static double area(double l, double m){
+    return l*m;
   }
 }
