1e207035ec5f451e54b55a106a2d030a16fc11ee
Rita Ryabova <riryabova@mail.ru>
2018-06-14 23:17:29 +0500

    2.2 исправление GroupCreationsTests для работы с версией мозилы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook;
 
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
