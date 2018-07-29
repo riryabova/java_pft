a6360bf8ad6dcbc636d1c9ecbe9a1ce37f0ebd10
Rita Ryabova <riryabova@mail.ru>
2018-07-29 13:10:32 +0500

    Задание №15: Реализовать проверку данных, загружаемых из БД Создание, удаление модфикация

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -43,10 +43,10 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation(GroupData group) {
 
     app.goTo().groupPage();
-    Groups before = app.group().all();
+    Groups before = app.db().groups();
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size() + 1));
-    Groups after = app.group().all();
+    Groups after = app.db().groups();
 
     assertThat(after.size(), equalTo(before.size() + 1));
     assertThat(after, equalTo(
