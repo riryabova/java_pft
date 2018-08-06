4011c16d0c3c66d855f2d5e049d34d572a77fdfe
Rita Ryabova <riryabova@mail.ru>
2018-08-06 08:29:03 +0500

    8.6. Получение почты на встроенный в тесты почтовый сервер (ошибки исправлены)

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -15,7 +15,7 @@ public class RegistrationHelper extends HelperBase{
     wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
     type(By.name("username"), username);
     type(By.name("email"), email);
-    click(By.xpath("//input[@value='Signup']"));
+    click(By.xpath("//input[@value='Зарегистрироваться']"));
     }
 
   public void finish(String confrimationLink, String password) {
--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -1,10 +1,12 @@
 <?php
-$g_hostname = 'localhost';
-$g_db_type  = 'mysqli';
-$g_database_name = 'bugtracker';
-$g_db_username = 'root';
-$g_db_password = '';
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+
+$g_default_timezone       = 'Europe/Berlin';
 $g_signup_use_captcha = OFF;
-$g_crypto_master_salt = 'qwertyqwertyqwer';
+$g_crypto_master_salt     = 'XIUhdPhWXHK1i4+WYTYcqBQDzIsDSI4N2ch+3XNW1IM=';
 $g_phpMailer_method = PHPMAILER_METHOD_SMTP;
 $g_smtp_host = 'localhost';
